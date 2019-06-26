80dffc6fd6054ea6aefeeabec480d19162e952f3
Paul Abernathy <paul.abernathy7@gmail.com>
2019-06-25 22:54:42 -0400

    added friends to state

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,9 +1,34 @@
 import React from "react";
 import logo from "./logo.svg";
+import { BrowserRouter as Router } from "react-router-dom";
+import axios from "axios";
+import Friend from "./Components/Friend";
 import "./App.css";
 
-function App() {
-  return <div className="App" />;
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      friends: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/friends")
+      .then(res => this.setState({ friends: res.data }))
+      .catch(err => {});
+  }
+
+  render() {
+    return (
+      <div>
+        {this.state.friends.map(item => {
+          return <Friend friend={item} />;
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
