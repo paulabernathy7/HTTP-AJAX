0872d4c6bf4c9d65dfe1f82854cfa25204f7ae92
Paul Abernathy <paul.abernathy7@gmail.com>
2019-06-30 13:30:31 -0400

    post request works

--- friends/src/Components/Form.js
+++ friends/src/Components/Form.js
@@ -11,14 +11,49 @@ class Form extends React.Component {
     };
   }
 
+  handleChange = e => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  };
+
+  addFriend = e => {
+    e.preventDefault();
+    //destructing the state below
+    // created new object called newFriend
+    const { name, age, email } = this.state;
+    const newFriend = { name, age, email };
+    axios
+      .post("http://localhost:5000/friends", newFriend)
+      .then(response => {
+        console.log(response.data);
+      })
+      .catch(err => {});
+  };
+
   render() {
     return (
       <div>
-        <form>
-          <input placeholder="name" value={this.state.name} name="name" />
-          <input placeholder="age" value={this.state.age} name="age" />
-          <input placeholder="email" value={this.state.email} name="email" />
-          <button type="submit" />
+        <form onSubmit={this.addFriend}>
+          <input
+            onChange={this.handleChange}
+            placeholder="name"
+            value={this.state.name}
+            name="name"
+          />
+          <input
+            onChange={this.handleChange}
+            placeholder="age"
+            value={this.state.age}
+            name="age"
+          />
+          <input
+            onChange={this.handleChange}
+            placeholder="email"
+            value={this.state.email}
+            name="email"
+          />
+          <button type="submit">submit</button>
         </form>
       </div>
     );
